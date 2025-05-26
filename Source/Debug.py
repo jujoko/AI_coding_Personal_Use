afcaf1c5f320175405c03060f6f2debd9dd699e0
jujoko1 <jujoko123456@gmail.com>
2025-05-26 13:15:16 +0900

    style/fix get_code method & add comments

--- Source/Debug.py
+++ Source/Debug.py
@@ -1,8 +1,11 @@
-class Debug: # Debugging utility class
-    def __init__(self, code_file=None):
+class Debug: # Debugging utility class #디버깅 역할을 할 클래스.
+    def __init__(self, code_file=None): # code_file: str, code file directory
+        self.code = ""  # code: str, code to debug
         if code_file is not None:
             with open(code_file, "r") as f: 
-                self.code = f.read() 
-        self.code_file = code_file
-    def get_code(self, code):
-        self.code_file = code
+                self.code = f.read()    
+        self.code_file = code_file  
+    def get_code(self): #디버깅 할 코드가 파일 형식이 아니라면 쓰는 메소드
+        code = input()  # Get code from user input
+        self.code = code
+        
