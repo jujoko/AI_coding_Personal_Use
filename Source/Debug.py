57c6ef7c1d0a3db05ce6794c490d8a85d7a3a1f1
jujoko1 <jujoko123456@gmail.com>
2025-05-28 14:38:04 +0900

    Revert "feat/Load LLM model "mistralai/Ministral-8B-Instruct-2410" in constructor"
    This reverts commit 80856b1e0b6334275ff8ff92156db028c78e2b21.

--- Source/Debug.py
+++ Source/Debug.py
@@ -5,14 +5,6 @@ class Debug: # Debugging utility class #디버깅 역할을 할 클래스.
             with open(code_file, "r") as f: 
                 self.code = f.read()    
         self.code_file = code_file  
-        
-        #예시 모델.
-        from vllm import LLM
-        from vllm.sampling_params import SamplingParams
-        model_name = "mistralai/Ministral-8B-Instruct-2410"
-        self.llm_params = SamplingParams(max_tokens=8192)
-        self.llm =  LLM(model=model_name, tokenizer_mode="mistral", config_format="mistral", load_format="mistral")
-    
     def get_code(self): #디버깅 할 코드가 파일 형식이 아니라면 쓰는 메소드
         code = input()  # Get code from user input
         self.code = code
