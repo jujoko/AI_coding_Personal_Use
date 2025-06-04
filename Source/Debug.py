528e51778e35dcb179261f543fdf19c448fb534f
jujoko1 <jujoko123456@gmail.com>
2025-06-05 03:52:49 +0900

    refactor/get_code 메서드를 사용자 입력 기반 인터랙션 방식으로 개선

--- Source/Debug.py
+++ Source/Debug.py
@@ -5,8 +5,20 @@ class Debug: # Debugging utility class #디버깅 역할을 할 클래스.
             with open(code_file, "r") as f: 
                 self.code = f.read()    
         self.code_file = code_file  
-    def get_code(self): #디버깅 할 코드가 파일 형식이 아니라면 쓰는 메소드
-        code = input()  # Get code from user input
+    def get_code(self): #code를 입력 받는 함수
+        code = ""
+        print("검사받을 코드를 한 행씩 입력해주세요. (종료하려면 '!STOP' 입력)")
+        while True:
+            tmp = input()
+            if not tmp.strip():    #빈 줄 건너뛰기.
+                continue
+            if tmp.strip()[0] == "!":   # '!'로 시작하는 경우, 명령어로 인식.
+                if tmp.strip()[1:] == "STOP":   # '!STOP' 입력 시, 입력 종료.
+                    break
+                if tmp.strip()[1:] == "CHECK":
+                    print(code)
+            else :
+                code += tmp + "\n"
         self.code = code
         
     def check_grammar(self):
